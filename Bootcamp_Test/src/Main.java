a0bac435ee23ff784ab8de716ad3dc06869d33b5
Andrea Llavel <andryllavel20@gmail.com>
2023-06-07 05:48:41 -0300

    Consigna 1

---
+++ Bootcamp_Test/src/Main.java
@@ -0,0 +1,11 @@
+//Andrea Llavel. 
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Lucas";
+        Integer nroEquipo = 2;
+        String texto = "Hola Soy"+ nombre+",del equipo nro."+nroEquipo;
+        System.out.println("Andrea Llavel");
+        System.out.println("Equipo 2");
+        System.out.println("Andefined");
+    }
+}
